310b1f1743ba8fa98489dfc7b1c3506ab934fc2f
stepan.rutz <stepan.rutz@gmx.de>
2025-12-25 12:14:21 +0100

    gits

--- src/nominatim.cpp
+++ src/nominatim.cpp
@@ -28,7 +28,6 @@ NominatimResult NominatimResult::fromJson(const QJsonObject &json)
         result.boundingbox.lonMin = bbox[2].toString().toDouble();
         result.boundingbox.lonMax = bbox[3].toString().toDouble();
     }
-
     return result;
 }
 
